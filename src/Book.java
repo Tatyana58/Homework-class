3781bcd126ebe6860af20106ff744b8fdb9e24c0
Tatyana58 <tatbir@rambler.ru>
2022-12-13 00:13:46 +0300

    Beginning создание класса Author создание класса Book

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,18 @@
 public class Book {
+    private String titleBook;
+    private int yearPublication;
+    public Book(String titleBook,int yearPublication) {
+        this.titleBook=titleBook;
+        this.yearPublication=yearPublication;
+    }
+    public String getTitleBook() {
+        return this.titleBook;
+    }
+    public int getYearPublication() {
+        return this.yearPublication;
+    }
+
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Домашняя работа - Классы");
-
+        Author nikolay = new Author("Николай", "Прохоренок");
+        System.out.println("nikolay.getName() = " + nikolay.getName());
+        System.out.println("nikolay.getSurname() = " + nikolay.getSurname());
+        Book bookName1 = new Book("Основы Java",2022);
+        System.out.println("bookName1.getTitleBook() = " + bookName1.getTitleBook());
+        System.out.println("bookName1.getYearPublication() = " + bookName1.getYearPublication());
     }
 }
