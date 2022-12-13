ea6fe4e1706635e6b170af378aeaa483e891ccee
Tatyana58 <tatbir@rambler.ru>
2022-12-13 23:05:03 +0300

    Beginning создание класса Author создание класса Book Вывели в консоль двух авторов двух книг

--- src/Book.java
+++ src/Book.java
@@ -1,9 +1,11 @@
 public class Book {
     private String titleBook;
     private int yearPublication;
+
     public Book(String titleBook,int yearPublication) {
         this.titleBook=titleBook;
         this.yearPublication=yearPublication;
+
     }
     public String getTitleBook() {
         return this.titleBook;
@@ -14,7 +16,4 @@ public class Book {
     public void setYearPublication(int yearPublication) {
         this.yearPublication=yearPublication;
     }
-
-
-
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
-import java.util.Arrays;
-
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Домашняя работа - Классы");
-        Author nikolay = new Author("Николай", "Прохоренок");
-        System.out.println("nikolay.getName() = " + nikolay.getName());
-        System.out.println("nikolay.getSurname() = " + nikolay.getSurname());
+        System.out.println("Домашняя работа - Классы\n");
+        Author author1 = new Author("Николай", "Прохоренок");
+        System.out.println("Автор: " + author1.getName()+" "+author1.getSurname());
         Book bookName1 = new Book("Основы Java",2022);
-        System.out.println("bookName1.getTitleBook() = " + bookName1.getTitleBook());
-        System.out.println("bookName1.getYearPublication() = " + bookName1.getYearPublication());
+        System.out.println("Название книги: " + bookName1.getTitleBook()+" год издания - "+bookName1.getYearPublication());
+        Author author2 = new Author("Анджел", "Леонард");
+        Book bookName2 = new Book("Java практические задачи",2021);
+        System.out.println("Автор: " + author2.getName()+" "+author2.getSurname());
+        System.out.println("Название книги: " + bookName2.getTitleBook()+" год издания - "+bookName2.getYearPublication());
     }
 }
