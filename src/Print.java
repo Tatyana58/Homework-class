3bb297bbabaf0f2ac934f9013b532807aaf5587f
Tatyana58 <tatbir@rambler.ru>
2022-12-14 21:13:56 +0300

    Beginning создание класса Author создание класса Book Вывели в консоль двух авторов двух книг Изменили год издания Имя и фамилию Автора пометили в одну переменную В Классе Book вызывается класс Author Создали класс Print и перенесли метод main в него

---
+++ src/Print.java
@@ -0,0 +1,15 @@
+public class Print {
+    public static void main(String[] args){
+        Author author1 = new Author("Николай", "Прохоренок");
+        Book bookName1 = new Book(new Author(author1.getName(), author1.getSurname()), "Основы Java", 2022);
+        Author author2 = new Author("Анджел","Леонард");
+        Book bookName2 = new Book(new Author(author2.getName(), author2.getSurname()),"Java Решение практических задач", 2021);
+        System.out.println("Автор, название книги, год издания: " + author1.getName() + " " + author1.getSurname() + " "
+                + bookName1.getTitleBook() + " " + bookName1.getYearPublication());
+        bookName1.setYearPublication(2020);
+        System.out.println("Автор, название книги, год издания: " + author1.getName() + " " + author1.getSurname() + " "
+                + bookName1.getTitleBook() + " " + bookName1.getYearPublication());
+        System.out.println("Автор, название книги, год издания: " + author2.getName() + " " + author2.getSurname() + " "
+                + bookName2.getTitleBook() + " " + bookName2.getYearPublication());
+    }
+}
